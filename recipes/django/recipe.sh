3ed68fa6f20c7488c3570f43c941932526b8baca
Paolo Patruno <pat1@localhost.localdomain>
2017-02-16 12:33:40 +0100

    remove setuptools as dependence

--- recipes/django/recipe.sh
+++ recipes/django/recipe.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 VERSION_django=${VERSION_django:-1.10.5}
-DEPS_django=(sqlite3 python)
-URL_django=https://pypi.python.org/packages/source/D/Django/Django-$VERSION_django.tar.gz
-#MD5_django=3ea7a00ea9e7a014e8a4067dd6466a1b
+DEPS_django=( sqlite3 python)
+URL_django=https://pypi.python.org/packages/c3/c2/6096bf5d0caa4e3d5b985ac72e3a0c795e37fa7407d6c85460b2a105b467/Django-1.10.5.tar.gz
+
+MD5_django=3fce02f1e6461fec21f1f15ea7489924
 BUILD_django=$BUILD_PATH/django/$(get_directory $URL_django)
 RECIPE_django=$RECIPES_PATH/django
 
@@ -26,6 +27,9 @@ function shouldbuild_django() {
 	if [ -d "$SITEPACKAGES_PATH/django" ]; then
 		DO_BUILD=0
 	fi
+
+	DO_BUILD=1
+
 }
 
 function build_django() {
