378fed11e254a6cb45f50174b4a63b0c21023141
pat1 <p.patruno@iperbole.bologna.it>
2017-04-29 14:45:17 +0200

    add comment for old django

--- recipes/django/recipe.sh
+++ recipes/django/recipe.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-VERSION_django=${VERSION_django:-1.10.5}
 DEPS_django=( sqlite3 python)
-URL_django=https://pypi.python.org/packages/c3/c2/6096bf5d0caa4e3d5b985ac72e3a0c795e37fa7407d6c85460b2a105b467/Django-1.10.5.tar.gz
+#DEPS_django=( sqlite3 python setuptools)
 
+#URL_django=https://pypi.python.org/packages/7f/a6/4ab15169e9075635c0a64bb0a569d1e02c7bd9b5ba839f05fe5d1fbbe3fb/Django-1.9.2.tar.gz
+#VERSION_django=${VERSION_django:-1.9.2}
+#MD5_django=ee90280973d435a1a6aa01b453b50cd1
+
+URL_django=https://pypi.python.org/packages/c3/c2/6096bf5d0caa4e3d5b985ac72e3a0c795e37fa7407d6c85460b2a105b467/Django-1.10.5.tar.gz
+VERSION_django=${VERSION_django:-1.10.5}
 MD5_django=3fce02f1e6461fec21f1f15ea7489924
+
 BUILD_django=$BUILD_PATH/django/$(get_directory $URL_django)
 RECIPE_django=$RECIPES_PATH/django
+
 
 function prebuild_django() {
 
